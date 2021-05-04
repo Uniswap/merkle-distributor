68a098f6a196e68582da514705002a6138cda6a8
Albert Su <albert_su@berkeley.edu>
2021-05-04 10:39:06 -0700

    Saved changes for git stash

---
+++ contracts/StakingHelpers/RewardsDistributionRecipient.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.4;
+
+// Inheritance
+import "./Owned.sol";
+
+// https://docs.synthetix.io/contracts/source/contracts/rewardsdistributionrecipient
+contract RewardsDistributionRecipient is Owned {
+    address public rewardsDistribution;
+
+    function notifyRewardAmount(uint256 reward) external;
+
+    modifier onlyRewardsDistribution() {
+        require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
+        _;
+    }
+
+    function setRewardsDistribution(address _rewardsDistribution) external onlyOwner {
+        rewardsDistribution = _rewardsDistribution;
+    }
+}
