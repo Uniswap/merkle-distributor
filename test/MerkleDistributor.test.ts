aaeb3375e2bd8e0d4f453e5ffe6735052cdd6cd3
AzFlin <azflin@users.noreply.github.com>
2022-06-29 15:40:46 -0400

    start test file

---
+++ test/MerkleDistributor.test.ts
@@ -0,0 +1,12 @@
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { waffle } from "hardhat";
+
+const hre = require("hardhat");
+
+describe("MerkleDistributor tests", () => {
+
+  it("should sanity check", async () => {
+    console.log("running!");
+  });
+});
