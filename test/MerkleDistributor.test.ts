e4890109fe7222b3979c6aafb901fc93a1b05013
AzFlin <azflin@users.noreply.github.com>
2022-06-30 14:02:08 -0400

    continue

--- test/MerkleDistributor.test.ts
+++ test/MerkleDistributor.test.ts
@@ -1,7 +1,7 @@
 import { ethers } from "hardhat"
 import chai, { expect } from "chai"
 import { solidity } from "ethereum-waffle"
-import { Contract, ContractFactory, BigNumber } from "ethers"
+import { Contract, ContractFactory, BigNumber, constants } from "ethers"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import BalanceTree from '../src/balance-tree'
 
@@ -19,9 +19,12 @@ describe("MerkleDistributor tests", () => {
   let distributorFactory: ContractFactory
   let wallet0: SignerWithAddress
   let wallet1: SignerWithAddress
+  let wallets: SignerWithAddress[]
 
   beforeEach(async () => {
-    [wallet0, wallet1] = await ethers.getSigners()
+    wallets = await ethers.getSigners()
+    wallet0 = wallets[0]
+    wallet1 = wallets[1]
     const tokenFactory = await ethers.getContractFactory("TestERC20", wallet0)
     token = await tokenFactory.deploy('Token', 'TKN', 0, overrides)
     distributorFactory = await ethers.getContractFactory("MerkleDistributor", wallet0)
@@ -172,6 +175,60 @@ describe("MerkleDistributor tests", () => {
         const tx = await distributor.claim(0, wallet0.address, 100, proof, overrides)
         const receipt = await tx.wait()
         expect(receipt.gasUsed).to.eq(81091)
+      })
+    })
+
+    describe('larger tree', () => {
+      let distributor: Contract
+      let tree: BalanceTree
+      beforeEach('deploy', async () => {
+        tree = new BalanceTree(
+          wallets.map((wallet, ix) => {
+            return { account: wallet.address, amount: BigNumber.from(ix + 1) }
+          })
+        )
+        distributor = await distributorFactory.deploy(token.address, tree.getHexRoot(), overrides)
+        await token.setBalance(distributor.address, 201)
+      })
+
+      it('claim index 4', async () => {
+        const proof = tree.getProof(4, wallets[4].address, BigNumber.from(5))
+        await expect(distributor.claim(4, wallets[4].address, 5, proof, overrides))
+          .to.emit(distributor, 'Claimed')
+          .withArgs(4, wallets[4].address, 5)
+      })
+
+      it('claim index 9', async () => {
+        const proof = tree.getProof(9, wallets[9].address, BigNumber.from(10))
+        await expect(distributor.claim(9, wallets[9].address, 10, proof, overrides))
+          .to.emit(distributor, 'Claimed')
+          .withArgs(9, wallets[9].address, 10)
+      })
+
+      it('gas', async () => {
+        const proof = tree.getProof(9, wallets[9].address, BigNumber.from(10))
+        const tx = await distributor.claim(9, wallets[9].address, 10, proof, overrides)
+        const receipt = await tx.wait()
+        expect(receipt.gasUsed).to.eq(84441)
+      })
+
+      it('gas second down about 15k', async () => {
+        await distributor.claim(
+          0,
+          wallets[0].address,
+          1,
+          tree.getProof(0, wallets[0].address, BigNumber.from(1)),
+          overrides
+        )
+        const tx = await distributor.claim(
+          1,
+          wallets[1].address,
+          2,
+          tree.getProof(1, wallets[1].address, BigNumber.from(2)),
+          overrides
+        )
+        const receipt = await tx.wait()
+        expect(receipt.gasUsed).to.eq(67341)
       })
     })
   })
