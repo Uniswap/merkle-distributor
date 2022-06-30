5155c54e54467d6d6f0f6fc343f069d11a0f668e
AzFlin <azflin@users.noreply.github.com>
2022-06-30 09:21:32 -0400

    start a test

--- test/MerkleDistributor.test.ts
+++ test/MerkleDistributor.test.ts
@@ -1,12 +1,14 @@
 import { ethers } from "hardhat";
 import { expect } from "chai";
-import { waffle } from "hardhat";
 
 const hre = require("hardhat");
 
 describe("MerkleDistributor tests", () => {
 
   it("should sanity check", async () => {
-    console.log("running!");
+    const [wallet0, wallet1] = await ethers.getSigners();
+    const tokenFactory = await ethers.getContractFactory("TestERC20", wallet0);
+    const token = await tokenFactory.deploy('Token', 'TKN', 0);
+    console.log(token);
   });
 });
