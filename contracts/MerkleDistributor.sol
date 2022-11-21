097193e5028d7ec845261add9b1d1b153bb1ac02
AzFlin <allenlin1992@hotmail.com>
2022-11-21 13:49:49 -0500

    Genie USDC Airdrop (#26)
    * Upgrade solidity to 0.8.15. Add hardhat. Add script to deploy MerkleDistributor.sol
    
    * add cache/ to gitignore
    
    * start test file
    
    * start a test
    
    * continue tests
    
    * continue
    
    * continue
    
    * continue
    
    * tentative complete refactor of tests
    
    * tentatively complete UniswapUSDCAirdrop
    
    * add tests for uniswap airdrop
    
    * Fix code style issues with Prettier
    
    * Edit scripts to use hardhat
    
    * update yml
    
    * Rename to MerkleDistributorWithDeadline.sol
    
    * add test 'only owner can withdraw even after end time'
    
    * start refactor to run all tests for MerkleDistributorWithDeadline
    
    * continue
    
    * continue
    
    * continue
    
    * continue
    
    * continue
    
    * Remove branches from lint.yml
    
    * Increase optimization runs. Use SafeERC20
    
    * Fix code style issues with Prettier
    
    * try reverting lint.yml to see if tests will run..
    
    * add end time condition to constructor
    
    * update deploy script
    
    * use custom errors in MerkleDistributor. Use newer version of ethers to accomodate these custom errors
    
    * Remove unneeded space in constructor
    
    * custom errors for MerkleDistributorWithDeadline
    
    * pin node 16
    
    * Fix code style issues with Prettier
    
    * fix compiler warnings
    
    * use 0.8.17
    
    Co-authored-by: AzFlin <azflin@users.noreply.github.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: Noah Zinsmeister <noahwz@gmail.com>

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -1,18 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity =0.6.11;
+pragma solidity =0.8.17;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/cryptography/MerkleProof.sol";
-import "./interfaces/IMerkleDistributor.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {IMerkleDistributor} from "./interfaces/IMerkleDistributor.sol";
+
+error AlreadyClaimed();
+error InvalidProof();
 
 contract MerkleDistributor is IMerkleDistributor {
+    using SafeERC20 for IERC20;
+
     address public immutable override token;
     bytes32 public immutable override merkleRoot;
 
     // This is a packed array of booleans.
     mapping(uint256 => uint256) private claimedBitMap;
 
-    constructor(address token_, bytes32 merkleRoot_) public {
+    constructor(address token_, bytes32 merkleRoot_) {
         token = token_;
         merkleRoot = merkleRoot_;
     }
@@ -31,16 +36,20 @@ contract MerkleDistributor is IMerkleDistributor {
         claimedBitMap[claimedWordIndex] = claimedBitMap[claimedWordIndex] | (1 << claimedBitIndex);
     }
 
-    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof) external override {
-        require(!isClaimed(index), 'MerkleDistributor: Drop already claimed.');
+    function claim(uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof)
+        public
+        virtual
+        override
+    {
+        if (isClaimed(index)) revert AlreadyClaimed();
 
         // Verify the merkle proof.
         bytes32 node = keccak256(abi.encodePacked(index, account, amount));
-        require(MerkleProof.verify(merkleProof, merkleRoot, node), 'MerkleDistributor: Invalid proof.');
+        if (!MerkleProof.verify(merkleProof, merkleRoot, node)) revert InvalidProof();
 
         // Mark it claimed and send the token.
         _setClaimed(index);
-        require(IERC20(token).transfer(account, amount), 'MerkleDistributor: Transfer failed.');
+        IERC20(token).safeTransfer(account, amount);
 
         emit Claimed(index, account, amount);
     }
