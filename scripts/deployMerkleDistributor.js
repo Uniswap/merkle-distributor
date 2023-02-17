7e0dcdfc6a3100c842b9e9dd9d96654af3953c87
Bouke Stam <bouke@allianceblock.io>
2023-02-17 10:14:53 +0100

    Removed hardcoded token

--- scripts/deployMerkleDistributor.js
+++ scripts/deployMerkleDistributor.js
@@ -5,9 +5,8 @@ const { ethers } = require('hardhat')
 async function main() {
   const MerkleDistributor = await ethers.getContractFactory('MerkleDistributor')
   const merkleDistributor = await MerkleDistributor.deploy(
-    // USDC
-    '0x8544789aAD4C7E2F982b9c9EEEa58caeA38b6bd1',
-    '0x1589a5da0a0f954ef5c7a21a4c199ee962e765c5aad2664745071eb51d9e180b'
+    process.env.DEPLOY_TOKEN,
+    process.env.DEPLOY_ROOT
   )
   await merkleDistributor.deployed()
   console.log(`merkleDistributor deployed at ${merkleDistributor.address}`)
--- scripts/deployMerkleDistributorWithDeadline.js
+++ scripts/deployMerkleDistributorWithDeadline.js
@@ -5,9 +5,8 @@ const { ethers } = require('hardhat')
 async function main() {
   const MerkleDistributorWithDeadline = await ethers.getContractFactory('MerkleDistributorWithDeadline')
   const merkleDistributorWithDeadline = await MerkleDistributorWithDeadline.deploy(
-    // USDC
-    '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    '0xbe154afea9ba1e08729654a19c53952a892d6b37fe0b5d1bdf8ac4f51d03a426',
+    process.env.DEPLOY_TOKEN,
+    process.env.DEPLOY_ROOT,
     1688493524
   )
   await merkleDistributorWithDeadline.deployed()
