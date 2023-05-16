aec658400eb104f4d9204f779e01cd18402ee5ab
debych <zh.eliseev@yandex.ru>
2023-05-16 17:35:02 +0300

    set mainnet Rubic address

--- scripts/deployMerkleDistributor.js
+++ scripts/deployMerkleDistributor.js
@@ -6,7 +6,7 @@ async function main() {
   const MerkleDistributor = await ethers.getContractFactory('MerkleDistributor')
   const merkleDistributor = await MerkleDistributor.deploy(
     // Token Address
-    '0x09f3bd68a90f10da92add1d14767340fbc1485eb',
+    '0x3330BFb7332cA23cd071631837dC289B09C33333',
     // Merkle root
     '0x584062b7d1ee3a0ea2112a10155154f1370541f2365dd5bd90c2fa2417e26ca9'
   )
