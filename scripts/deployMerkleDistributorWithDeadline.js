fcae5cd14f5fef6df1853f6fe5a6f00e1be5734f
Seth Hrbek <seth.hrbek@gmail.com>
2023-06-12 13:03:54 -0400

    add generic providers

--- scripts/deployMerkleDistributorWithDeadline.js
+++ scripts/deployMerkleDistributorWithDeadline.js
@@ -6,12 +6,19 @@ const { ethers } = require('hardhat')
 
 const MERKLE_ROOT = '0xa106777ce21246bc8d8b5389a36243ea5584429313ea07e6592c3173c5e226d7'
 const SARCO_TOKEN_ADDRESS = '0x4633b43990b41B57b3678c6F3Ac35bA75C3D8436'
+
+const unixTimestampsYearsFromNow = (numOfYears) => {
+  let now = new Date();
+  let futureDate = new Date(now.setFullYear(now.getFullYear() + numOfYears));
+  return Math.floor(futureDate.getTime() / 1000);
+}
+
 async function main() {
   const MerkleDistributorWithDeadline = await ethers.getContractFactory('MerkleDistributorWithDeadline')
   const merkleDistributorWithDeadline = await MerkleDistributorWithDeadline.deploy(
     SARCO_TOKEN_ADDRESS,
     MERKLE_ROOT,
-    1686247988 // June 8th
+    unixTimestampsYearsFromNow(2)
   )
   await merkleDistributorWithDeadline.deployed()
   console.log(`merkleDistributorWithDeadline deployed at ${merkleDistributorWithDeadline.address}`)
